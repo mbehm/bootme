476cf3f25d0ea1831e6c2777ca19e9b37863ccac
Markus Behm <markus@iwa.fi>
2014-10-18 04:06:01 +0300

    Minor i18n fixes

--- client/i18n/en.i18n.js
+++ client/i18n/en.i18n.js
@@ -1,7 +1,10 @@
 i18n.map('en', {
 	app: {
-		title: 'Timetracker',
+		title: 'Bootme',
 		toggle_navigation: 'Toggle navigation'
+	},
+	error: {
+		page_not_found: 'The page you are looking for cannot be found'
 	},
 	users: {
 		please_login_or_register: 'Please login or register to use this application'
