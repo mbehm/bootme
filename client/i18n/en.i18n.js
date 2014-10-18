b3dda4adabe36ebc7874e40ca827e540dd129bdc
Markus Behm <markus@iwa.fi>
2014-10-18 09:50:30 +0300

    Changed translations to have better keys
    Conflicts:
    	client/i18n/en.i18n.js

--- client/i18n/en.i18n.js
+++ client/i18n/en.i18n.js
@@ -1,15 +1,25 @@
 i18n.map('en', {
 	app: {
 		title: 'Bootme',
-		toggle_navigation: 'Toggle navigation'
 	},
-	home: {
-		brand_new_site: 'Your brand new site!'
+	buttons: {
+		toggle: 'Toggle',
+		add: 'Add',
+		close: 'Close',
+		cancel: 'Cancel',
+		ok: 'Ok',
+		create: 'Create'
+	},
+	navigation: {
+		toggle: 'Toggle navigation'
 	},
 	error: {
 		page_not_found: 'The page you are looking for cannot be found'
 	},
-	users: {
+	home: {
+		brand_new_site: 'Your brand new site!'
+	},
+	login: {
 		please_login_or_register: 'Please login or register to use this application'
 	}
 });
